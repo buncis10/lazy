01998f72937d5888bc6982663d39c867383cb75b
buncis mamen <buncismamen@gmail.com>
2017-08-30 23:22:56 +0700

    balikin ke versi crna awal biar ga rusak

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
-// export const API_URL = `http://api-lazy.herokuapp.com/`;
-export const API_URL = `http://192.168.0.19:3000/`;
+export const API_URL = `http://api-lazy.herokuapp.com/`;
+//export const API_URL = `http://192.168.0.19:3000/`;
