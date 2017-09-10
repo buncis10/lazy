dc79530210223fef9222dbdc12771dcbc65328f2
buncis mamen <buncismamen@gmail.com>
2017-09-10 19:33:11 +0700

    nambahin fcm

--- android/app/src/main/java/com/lazy/MainApplication.java
+++ android/app/src/main/java/com/lazy/MainApplication.java
@@ -3,6 +3,7 @@ package com.lazy;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.evollu.react.fcm.FIRMessagingPackage;
 import com.reactnative.photoview.PhotoViewPackage;
 import cl.json.RNSharePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FIRMessagingPackage(),
             new PhotoViewPackage(),
             new RNSharePackage(),
             new LinearGradientPackage(),
