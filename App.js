59743c3caed7c21f50a40ff3a6214ff89c3bbd51
buncis mamen <buncismamen@gmail.com>
2017-09-11 19:11:22 +0700

    benerin git ignore

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux';
 import reducers from './src/reducers/index'; 
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';   
+import FCM, {FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType} from 'react-native-fcm';
 
 const store = createStore(
   reducers,
@@ -15,6 +16,24 @@ const store = createStore(
 );
 
 export default class App extends React.Component {
+  componentDidMount() {
+    FCM.requestPermissions(); // for iOS
+    FCM.getFCMToken().then(token => {
+        console.log(token)
+        // store fcm token in your server
+    });
+    
+    this.notificationListener = FCM.on(FCMEvent.Notification, (notif) => {
+        // do some component related stuff
+        console.log("notif masuk")
+        console.log(notif)
+    });
+  }
+
+  componentWillUnmount() {
+      // stop listening for events
+      this.notificationListener.remove();
+  }
 
   render() {
     return (
