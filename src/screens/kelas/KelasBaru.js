e94265d6596ea9704fb045fdbc9626f43837f10b
angga <antoniusanggakurniawan@gmail.com>
2017-09-12 15:21:29 +0700

    instal react native material design ah

--- src/screens/kelas/KelasBaru.js
+++ src/screens/kelas/KelasBaru.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Alert, Text, View, Button, Image, TouchableHighlight, ScrollView } from 'react-native';
+import { StyleSheet, Alert, Text, View, Button, TouchableHighlight, ScrollView } from 'react-native';
 import TextField from 'react-native-md-textinput';
 import { Dropdown } from 'react-native-material-dropdown';
+import { Row, Subtitle, Image, Icon } from '@shoutem/ui';
 
 export default class KelasBaru extends React.Component {
   
@@ -15,12 +16,21 @@ export default class KelasBaru extends React.Component {
   
   render() {
     let kategori = [{
-        value: 'Banana',
+        value: 'Ipa',
       }, {
-        value: 'Mango',
+        value: 'Matematika',
       }, {
-        value: 'Pear',
+        value: 'Olahraga',
       }];
+
+    let jenjang = [{
+            value: 'SD',
+        }, {
+            value: 'SMP',
+        }, {
+            value: 'SMA',
+        }];
+      
     return (
       <View style={styles.container}>
         <ScrollView>
@@ -28,18 +38,35 @@ export default class KelasBaru extends React.Component {
                 <TextField style={styles.textInput} 
                     label={'Nama kelas'}
                     highlightColor= {'#56CE00'}
-                    textColor= {'grey'} />
-                <Dropdown
-                    label='Kategori kelas'
-                    data={kategori} />
+                    textColor= {'#9E9E9E'} />
                 <TextField style={styles.textInput}
                     label={'Deskripsi kelas'} 
                     highlightColor= {'#56CE00'}
-                    textColor= {'grey'}
+                    textColor= {'#9E9E9E'}
                     multiline={true}/>
+                <Dropdown
+                    label='Kategori kelas'
+                    data={kategori}
+                    textColor= {'#9E9E9E'} />
+                <Dropdown
+                    label='Jenjang pendidikan'
+                    data={jenjang}
+                    textColor= {'#9E9E9E'} />
                 <TextField style={styles.textInput}
                     label={'Tag'} highlightColor= {'#56CE00'}
-                    textColor= {'grey'} />
+                    textColor= {'#9E9E9E'} />
+                <View style={styles.gambarKelas}>
+                    <Text style={{fontWeight: 'bold', color: '#9E9E9E'}}>Gambar Kelas</Text>
+                    <Row>
+                        <Icon style={{fontSize: 100}}
+                            name="photo"
+                        />
+                        <Button 
+                            onPress={this._onPressButton}
+                            styleName="top"
+                            title="BROWSE" />
+                    </Row>
+                </View>
             </View>
         </ScrollView>
       </View>
@@ -57,6 +84,9 @@ const styles = StyleSheet.create({
       marginLeft: 20,
       marginRight: 20
   },
+  gambarKelas: {
+      
+  }
 });
 
 
